58536db52261e638783fa0dead33be66229a5909
Tevin Zhuo <73798707+teazhi@users.noreply.github.com>
2023-07-11 21:32:46 -0400

    Updated frontend

--- frontend/components/hero.tsx
+++ frontend/components/hero.tsx
@@ -29,9 +29,12 @@ export default function Hero() {
 
           {/* Section header */}
           <div className="text-center pb-12 md:pb-16">
-            <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">Make your website <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">wonderful</span></h1>
+            <h1 className="text-3xl md:text-4xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">
+                The only social media dashboard you need.
+            </h1>
             <div className="max-w-3xl mx-auto">
-              <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.</p>
+              <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">
+                Keep up with <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">messages.</span></p>
               <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
                 <div>
                   <a className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0" href="#0">Start free trial</a>
@@ -42,17 +45,6 @@ export default function Hero() {
               </div>
             </div>
           </div>
-
-          {/* Hero image */}
-          <ModalVideo
-            thumb={VideoThumb}
-            thumbWidth={768}
-            thumbHeight={432}
-            thumbAlt="Modal video thumbnail"
-            video="/videos/video.mp4"
-            videoWidth={1920}
-            videoHeight={1080} />
-
         </div>
 
       </div>
--- frontend/components/ui/header.tsx
+++ frontend/components/ui/header.tsx
@@ -23,22 +23,22 @@ export default function Header() {
   }, [top])
 
   return (
-    <header className={`fixed w-full z-30 md:bg-opacity-90 transition duration-300 ease-in-out ${!top ? 'bg-white backdrop-blur-sm shadow-lg' : ''}`}>
-      <div className="max-w-6xl mx-auto px-5 sm:px-6">
-        <div className="flex items-center justify-between h-16 md:h-20">
-
+    <header>
+      <div className="max-w-md mx-auto px-5 sm:px-6">
+        <div className="flex items-center justify-between h-20 md:h-32">
           {/* Site branding */}
-          <div className="shrink-0 mr-4">
+          {/* <div className="shrink-0 mr-4">
             <Logo />
-          </div>
+          </div> */}
 
           {/* Desktop navigation */}
-          <nav className="hidden md:flex md:grow">
+          <nav className="leading-loose hidden md:flex md:grow">
             {/* Desktop sign in links */}
-            <ul className="flex grow justify-end flex-wrap items-center">
+            <ul className="bg-gray-901 grow rounded-full flex justify-center flex-wrap items-center">
               <li>
                 <Link href="/signin" className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out">Sign in</Link>
               </li>
+              <li className='font-semibold text-2xl text-orange-100'>smdash</li>
               <li>
                 <Link href="/signup" className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3">
                   <span>Sign up</span>
