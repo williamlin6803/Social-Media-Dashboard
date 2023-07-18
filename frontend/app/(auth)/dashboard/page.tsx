2de637f57860e58ba163f1ceeabdbafe5cadf0e7
Tevin Zhuo <73798707+teazhi@users.noreply.github.com>
2023-07-18 13:17:47 -0400

    Update

--- frontend/app/(auth)/dashboard/page.tsx
+++ frontend/app/(auth)/dashboard/page.tsx
@@ -1,5 +1,5 @@
 export const metadata = {
-    title: 'Sign In | smdash',
+    title: 'Dashboard | smdash',
     description: 'Page description',
   }
   
--- frontend/app/(auth)/signup/page.tsx
+++ frontend/app/(auth)/signup/page.tsx
@@ -1,3 +1,12 @@
+/*
+  Server Component shouldn't contain client-specific code, for example, hooks such as useState. 
+  If you need that, you should add "use client" at the top, to make it a Client Component, which, 
+  obviously, shouldn't contain server-side stuff like metadata.
+
+  https://stackoverflow.com/questions/74965849/youre-importing-a-component-that-needs-usestate-it-only-works-in-a-client-comp
+*/
+"use client";
+
 import React, { useState } from 'react';
 const axios = require('axios');
 
@@ -12,6 +21,7 @@ export default function SignUp() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [signupMessage, setSignupMessage] = useState('');
   
   // Handle sign up form submission, which is invoked when the signup button is called: <form onSubmit={handleSignUp}>
   const handleSignUp = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -27,15 +37,17 @@ export default function SignUp() {
 
       // Handle successful signup response (e.g., show success message)
       console.log(response.data);
+      setSignupMessage('Sign up successful!'); // Display a success message
     } catch (error) {
       // Handle signup error (e.g., show error message)
       console.error(error);
+      setSignupMessage('Sign up failed. Please try again.'); // Display an error message
     }
   };
 
   // Rendering sign up form
   return (
-    <section className="bg-gradient-to-b from-gray-100 to-white">
+    <section className="bg-gradient-to-b from-gray-900 to-gray-901">
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <div className="pt-12 pb-12 md:pt-20 md:pb-20">
           <div className="max-w-2xl mx-auto text-center pb-6 md:pb-6">
@@ -46,7 +58,7 @@ export default function SignUp() {
             <form onSubmit={handleSignUp}>
               <div className="flex flex-wrap -mx-3 mb-4">
                 <div className="w-full px-3">
-                  <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="name">Name <span className="text-red-600">*</span></label>
+                  <label className="block text-white text-sm font-medium mb-1" htmlFor="name">Name <span className="text-red-600">*</span></label>
                   <input
                     id="name"
                     type="text"
@@ -60,7 +72,7 @@ export default function SignUp() {
               </div>
               <div className="flex flex-wrap -mx-3 mb-4">
                 <div className="w-full px-3">
-                  <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="email">Email <span className="text-red-600">*</span></label>
+                  <label className="block text-white text-sm font-medium mb-1" htmlFor="email">Email <span className="text-red-600">*</span></label>
                   <input
                     id="email"
                     type="email"
@@ -74,7 +86,7 @@ export default function SignUp() {
               </div>
               <div className="flex flex-wrap -mx-3 mb-4">
                 <div className="w-full px-3">
-                  <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="password">Password <span className="text-red-600">*</span></label>
+                  <label className="block text-white text-sm font-medium mb-1" htmlFor="password">Password <span className="text-red-600">*</span></label>
                   <input
                     id="password"
                     type="password"
@@ -95,6 +107,12 @@ export default function SignUp() {
                 By creating an account, you agree to the <a className="underline" href="#0">terms & conditions</a>, and our <a className="underline" href="#0">privacy policy</a>.
               </div>
             </form>
+
+            {signupMessage && (
+              <div className="text-white text-center mt-4">
+                {signupMessage}
+              </div>
+            )}
 
             <div className="text-gray-600 text-center mt-6">
               Already using smdash? <Link href="/signin" className="text-orange-100 hover:underline transition duration-150 ease-in-out">Sign in</Link>
