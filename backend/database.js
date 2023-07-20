4a3a93f4dab31b637bd33924cec548dda6cea1d7
Tevin Zhuo <73798707+teazhi@users.noreply.github.com>
2023-07-19 22:34:03 -0400

    .

--- backend/database.js
+++ backend/database.js
@@ -1,15 +1,12 @@
 const mongoose = require('mongoose');
 
-// Establish connection with MongoDB Atlas database
-const connectDatabase = () => {
-    mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
+const connectDatabase = async () => {
+    try {
+        await mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
         console.log('Connected to database');
-    })
-    .catch((error) => {
-        console.log('Error connecting to MangoDB database: ', error.message);
-    });
+    } catch (error) {
+        console.log('Error connecting to MongoDB database: ', error.message);
+    }
 };
 
-// Export the connectDatabase function to be used in other files
 module.exports = connectDatabase;
--- backend/server.js
+++ backend/server.js
@@ -2,29 +2,39 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const connectDatabase = require('./database');
 const mongoose = require('mongoose');
-const PORT = process.env.PORT || 4000;
-const userController = require('./controllers/user_controller');
-const userDatabase = require('./testuserdatabase');
 const userRoutes = require('./routes/user_routes');
+const PORT = process.env.PORT || 3000;
+
+// connectDatabase();
+
+(async () => {
+    try {
+        console.log('Testing')
+        await connectDatabase();
+    } catch (error) {
+        console.error('Failed to connect to the database', error.message);
+        throw error;
+    }
+})();
 
 // Enable CORS on all routes
-app.use(cors({
-    origin: 'https://social-media-dashboard-seven-pink.vercel.app/',
-    methods: ['POST', 'GET'],
-    credentials: true
-}));
+app.use(cors(
+    {
+        origin: ["https://social-ntw-frontend.vercel.app"],
+        methods: ['POST', 'GET', 'OPTIONS'],
+        credentials: true
+    }
+));
 
 app.use(express.json());
 
-// Connect to MongoDB
-mongoose.connect('mongodb+srv://williamlin6803:gk0KNs9V9F5zRRFB@cluster0.bmghano.mongodb.net/AllUsers?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected'))
-    .catch((err) => console.log('MongoDB connection error:', err));
-
-app.use('/api', userRoutes);
+app.get("/", (req, res) => {
+    res.json("Hello");
+})
+app.use(userRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
-
